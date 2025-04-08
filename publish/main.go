6cd57f7754eb7835d04f178fd4deb375288ec51d
Kar <438577872@qq.com>
2025-04-08 16:44:04 +0800

    feat: first commit

--- publish/main.go
+++ publish/main.go
@@ -20,21 +20,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(db)
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		// language=postgresql
 		reader.ReadLine()
-		db.Transaction(func(tx *gorm.DB) error {
-			var t = time.Now().UnixMilli()
-			marshal, _ := json.Marshal(t)
-			fmt.Println(string(marshal))
+		// language=postgresql
+		var t = time.Now().UnixMilli()
+		marshal, _ := json.Marshal(t)
+		err = db.Transaction(func(tx *gorm.DB) error {
 			tx.Exec(fmt.Sprintf("NOTIFY events, '%s'", marshal))
-			if t%2 == 0 {
-				return gorm.ErrRecordNotFound
-			}
 			return nil
 		})
+		if err != nil {
+			panic(err)
+		}
 
 	}
 }
